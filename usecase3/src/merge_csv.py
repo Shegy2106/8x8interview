845d253114fa36f3570a169973827a8b9294eb58
Darko Segvic <darko.segvic@gmail.com>
2023-07-26 11:46:19 +0200

    usecase1 - error handling

--- usecase3/src/merge_csv.py
+++ usecase3/src/merge_csv.py
@@ -2,15 +2,28 @@ import pandas as pd
 import sys
 import os
 
-file_dir = os.path.dirname(__file__)
+file_dir = os.path.abspath(os.path.dirname(__file__))
 
 def merge_csv():
-    df1 = pd.read_csv(os.path.join(file_dir,"../dataset/csv1.csv"))
-    df2 = pd.read_csv(os.path.join(file_dir,"../dataset/csv2.csv"))
+    csv1_path = os.path.join(file_dir,"../dataset/csv1.csv")
+    if not os.path.exists(csv1_path):
+        print(f"File {csv1_path} does not exist")
+
+    csv2_path = os.path.join(file_dir,"../dataset/csv2.csv")
+    if not os.path.exists(csv2_path):
+        print(f"File {csv2_path} does not exist")
+
+    df1 = pd.read_csv(csv1_path)
+    df2 = pd.read_csv(csv2_path)
     
     result_df = pd.merge(df1, df2, on='name')
     
-    result_df.to_csv(os.path.join(file_dir,"../dataset/csv3.csv"))
+    csv3_path = os.path.join(file_dir,"../dataset/csv3.csv")
+
+    try:
+        result_df.to_csv(csv3_path)
+    except Exception as ex:
+        print(ex)
 
 if __name__ == "__main__":
     merge_csv()
